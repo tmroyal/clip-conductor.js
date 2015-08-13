60b0a5c8c7853aa7f365f9ac0abb8d9e151ce294
tmroyal <thomas.m.royal@gmail.com>
2015-08-13 17:02:30 -0400

    Completed Player.js

--- src/Player.js
+++ src/Player.js
@@ -1,9 +1,37 @@
-function Player(audioContext, scheduler, requestObject){
-  scheduler.observe(this.playSound);
+function Player(audioContext, scheduler, server){
+  var self = this;
+  this.server = server;
+  this.sounds = {};
+  this.audioContext = audioContext;
+  scheduler.observe(this.playSound.bind(this));
 }
 
-Player.prototype.playSound = function(fileName){
-  console.log(fileName);
+Player.prototype.playSound = function(fileInfo, time){
+  if (this.sounds[fileInfo.handle]){
+    this.sounds[fileInfo.handle].start(time);
+  }
+};
+
+Player.prototype.loadFile = function(fileInfo, done, error){
+  return this.server.loadFile(fileInfo.filename)
+
+  .then(function(data){
+
+    this.audioContext.decodeAudioData(data, function(buffer){
+
+      this.sounds[fileInfo.handle] = 
+        this.audioContext.createBufferSource();
+      this.sounds[fileInfo.handle].buffer = buffer;
+      this.sounds[fileInfo.handle]
+          .connect(this.audioContext.destination);
+      
+      if (done){ done(); }
+
+    }.bind(this), error);
+
+  }.bind(this))
+
+  .catch(error);
 };
 
 module.exports = Player;
--- src/Player_test.js
+++ src/Player_test.js
@@ -1,44 +1,178 @@
 var Player;
-var scheduler, audioContext, requestObject, player;
+var scheduler, audioContext, server, player;
 
 beforeEach(function(){
+  scheduler = {
+    observe: function(){}
+  }
   Player = require('./Player');
 });
 
 describe('Player', function(){
-  it('should observe scheduler', function(){
-    scheduler = {
-      observe: sinon.spy()
-    }
-    player = new Player(audioContext, scheduler, requestObject);
 
-    scheduler.observe.called.should.be.true;
-  });
-
-  it('should call playSound callback', function(){
-    var stub = sinon.stub(Player.prototype, 'playSound'); 
-
-    scheduler = {
-      observe: function(cb){
-        this.subscription = cb;
-      },
-      test: function(){
-        this.subscription('playSound');
+  describe('new Player()', function(){
+    it('should observe scheduler', function(){
+      scheduler = {
+        observe: sinon.spy()
       }
-    };
-    
-    player = new Player(audioContext, scheduler, requestObject);
+      player = new Player(audioContext, scheduler, server);
 
-    scheduler.test();
-   
-    stub.called.should.be.true; 
-    stub.calledWith('playSound').should.be.true;
+      scheduler.observe.called.should.be.true;
+    });
+
+    it('should play sound on scheduler event', function(){
+      var sound = {
+        start: sinon.spy()
+      };
+      var fileInfo = { handle: 'testSound' };
+
+      scheduler = {
+        observe: function(cb){
+          this.subscription = cb;
+        },
+        test: function(){
+          this.subscription(fileInfo, 3);
+        }
+      };
       
+      player = new Player(audioContext, scheduler, server);
+      player.sounds['testSound'] = sound;
+      
+      scheduler.test();
+     
+      sound.start.calledWith(3).should.be.true;
+        
+    });
   });
 
-  it('should load given files');
-  it('should call a success when files load');
-  it('should call a fail callback when a file has failed to load');
+  describe('loadFile',function(){
+    var fileInfo, audioContext, server, player;
+    
+    beforeEach(function(){
+      fileInfo = {
+        filename: 'test.mp3',
+        handle: 'test'
+      };
 
-  it('should call a callback indicating progression of file download');
+      audioContext = {
+        decodeAudioData: function(data, cb){
+          cb(); 
+        },
+        createBufferSource: function(){
+          return {
+            connect: function(destination){
+            }
+          }
+        },
+        destination: 'audio context destination'
+      };
+
+      server = {
+        loadFile: function(fn){
+          return new Promise(function(resolve){ 
+            resolve('audio data');
+          });
+        }
+      };
+    });
+
+    it('should call server.loadfile with fn', function(){
+      var spy = sinon.spy();
+
+      server.loadFile = function(fn){
+        spy(fn);
+        return new Promise(function(){});
+      };
+
+        
+      player = new Player(audioContext, scheduler, server);
+      player.loadFile(fileInfo);
+
+      spy.calledWith(fileInfo.filename).should.be.true;
+    });
+
+    it('should call context.decodeAudioData', function(){
+      audioContext.decodeAudioData = sinon.spy();
+
+      server.loadFile = function(fn){
+        return new Promise(function(resolve){ 
+          resolve('audio data'); 
+        });
+      };
+
+      player = new Player(audioContext, scheduler, server);
+      return player.loadFile(fileInfo).then(function(){ 
+        audioContext.decodeAudioData.calledWith('audio data')
+          .should.be.true;
+      });
+    });
+
+    it('should create and connect buffer stored on this.sounds',
+      function(){
+        var spy = sinon.spy();
+
+        audioContext.decodeAudioData = function(data, cb){
+          cb('the buffer');
+        }
+
+        audioContext.createBufferSource = function(){
+          return {
+            connect: spy
+          };
+        };
+
+        player = new Player(audioContext, scheduler, server);
+
+        return player.loadFile(fileInfo).then(function(){
+          player.sounds[fileInfo.handle].buffer
+            .should.equal('the buffer');
+          player.sounds[fileInfo.handle].connect
+            .calledWith(audioContext.destination)
+            .should.be.true;
+        }); 
+      }
+    );
+
+    it('should call done callback on success', function(){
+      var spy = sinon.spy();
+      player = new Player(audioContext, scheduler, server);
+
+      return player.loadFile(fileInfo, spy).then(function(){
+        spy.called.should.be.true;
+      });
+
+    });
+
+    it('should call error callback on encoding error', function(){
+      var spy = sinon.spy();
+
+      audioContext.decodeAudioData = function(data, succ, er){
+        er();
+      };
+      player = new Player(audioContext, scheduler, server);
+
+      return player.loadFile(fileInfo, null, spy).then(function(){
+        spy.called.should.be.true;
+      });
+    });
+
+    it('should call error callback on network error', function(){
+      var spy = sinon.spy();
+
+      server.loadFile = function(fn){
+        return new Promise(function(resolve, reject){
+          reject();
+        });
+      };
+
+      player = new Player(audioContext, scheduler, server);
+
+      return player.loadFile(fileInfo, null, spy).then(function(){
+        spy.called.should.be.true;
+      });
+
+    });
+
+    
+  });
 });
