5605fcc4c873b0d0a2266951f48b817630bd916f
tmroyal <thomas.m.royal@gmail.com>
2015-08-14 15:14:16 -0400

    Build main.js

--- src/main.js
+++ src/main.js
@@ -1,2 +1,26 @@
+
 function ClipConductor(dependencies){
+  Server = dependencies.Server || require('./Server');
+  Player = dependencies.Player || require('./Player');
+  Scheduler = dependencies.Scheduler || require('./Scheduler');
+  AudioContext = dependencies.AudioContext || AudioContext;
+
+  this.audioContext = new AudioContext();
+  this.server = new Server();
+  this.scheduler = new Scheduler();
+  this.player = new Player(audioContext, scheduler, server);
 } 
+
+ClipConductor.prototype.addSound = function(msg, soundInfo){
+  return this.player.loadFile(soundInfo)
+  .then(function(){
+    this.scheduler.on(msg, soundInfo);
+  })
+  .catch(function(){
+    console.error('ClipConductor: there was a problem loading '+soundInfo.filename);
+  });
+};
+
+ClipConductor.prototype.trigger = function(msg){
+  this.scheduler.trigger(msg);
+};
