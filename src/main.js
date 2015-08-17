238c3641e62ebbcb6e0eef5b3feb0909cc24bdfc
tmroyal <thomas.m.royal@gmail.com>
2015-08-17 19:26:22 -0400

    main uses Object.create() pattern

--- src/main.js
+++ src/main.js
@@ -2,26 +2,35 @@ var Server = require('./Server');
 var Player = require('./Player'); 
 var Scheduler = require('./Scheduler');
 
-function ClipConductor(){
-  this.audioContext = new AudioContext();
-  this.server = new Server();
-  this.scheduler = new Scheduler();
-  this.player = new Player(this.audioContext, this.scheduler, this.server);
-} 
+var ClipConductor = {
+  new: function(){
+    var cond = Object.create(this);
+    cond.audioContext = new AudioContext();
+    cond.server = Server.new();
+    cond.scheduler = Scheduler.new();
+    cond.player = Player.new(
+      cond.server,
+      cond.scheduler,
+      cond.server
+    );
+    
+    return cond; 
+  },
 
-ClipConductor.prototype.addSound = function(msg, soundInfo){
-  return this.player.loadFile(soundInfo)
-  .then(function(){
-    this.scheduler.on(msg, soundInfo);
-  }.bind(this))
-  .catch(function(er){
-    console.error('ClipConductor: there was a problem loading '+soundInfo.filename);
-    console.error(er);
-  });
-};
+  addSound: function(msg, soundInfo){
+    return this.player.loadFile(soundInfo)
+    .then(function(){
+      this.scheduler.on(msg, soundInfo);
+    }.bind(this))
+    .catch(function(er){
+      console.error('ClipConductor: there was a problem loading '+soundInfo.filename);
+      console.error(er);
+    });
+  },
 
-ClipConductor.prototype.trigger = function(msg){
-  this.scheduler.trigger(msg, 0);
+  trigger: function(msg){
+    this.scheduler.trigger(msg, 0);
+  }
 };
 
 module.exports = ClipConductor;
