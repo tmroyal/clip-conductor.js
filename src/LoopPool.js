31666d183d76082ba3e489b2462d96be0aa68aa8
tmroyal <thomas.m.royal@gmail.com>
2015-08-17 19:48:01 -0400

    Added looppool inheritance test

--- src/LoopPool.js
+++ src/LoopPool.js
@@ -2,4 +2,10 @@ var Scheduler = require('./Scheduler');
 
 var LoopPool = Object.create(Scheduler);
 
+LoopPool.new = function(name){
+  var lp = Scheduler.new.call(this);
+  lp.name = name;
+  return lp;
+};
+
 module.exports = LoopPool;
--- src/LoopPool_test.js
+++ src/LoopPool_test.js
@@ -1,7 +1,19 @@
 var Scheduler = require('./Scheduler');
 var LoopPool = require('./LoopPool');
 
+var loopPool;
+
+beforeEach(function(){
+  loopPool = LoopPool.new();
+});
+
 describe('LoopPool', function(){
   it('should inherit from scheduler', function(){
+    var scheduler = Scheduler.new();
+    for (prop in scheduler){
+      if (scheduler.hasOwnProperty(prop)){
+        loopPool.hasOwnProperty(prop).should.be.true;
+      }
+    }
   });
 });
