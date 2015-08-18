2e24e57012fe98dbe47aaec79981a67957ba57a7
tmroyal <thomas.m.royal@gmail.com>
2015-08-18 09:18:56 -0400

    Added functionality to main.js

--- src/main.js
+++ src/main.js
@@ -2,29 +2,42 @@ var Server = require('./Server');
 var Player = require('./Player'); 
 var Scheduler = require('./Scheduler');
 
-var ClipConductor = function(){
-  var server = Server.new();
-  var scheduler = Scheduler.new();
-  var player = Player.new( new AudioContext(), scheduler, server);
+var ClipConductor = {
+  new: function(provServer, provScheduler, provPlayer, provAudio){
 
-  var cond = {};
+    var server = (provServer ? provServer : Server).new();
+    var scheduler = (provScheduler ? provScheduler : Scheduler).new();
+    var audioContext = provAudio ? provAudio : AudioContext; 
+    var player = (provPlayer ? provPlayer : Player).new( 
+        new audioContext(), scheduler, server);
 
-  cond.addSound = function(msg, soundInfo){
-    return player.loadFile(soundInfo)
-    .then(function(){
-      scheduler.on(msg, soundInfo);
-    }.bind(this))
-    .catch(function(er){
-      console.error('ClipConductor: there was a problem loading '+soundInfo.filename);
-      console.error(er);
-    });
-  };
+    var cond = {};
 
-  cond.trigger = function(msg){
-    this.scheduler.trigger(msg, 0);
+    cond.addSound = function(msg, soundInfo){
+      return player.loadFile(soundInfo)
+      .then(function(){
+        scheduler.on(msg, soundInfo);
+      }.bind(this))
+      .catch(function(er){
+        console.error('ClipConductor: there was a problem loading '+soundInfo.filename);
+        console.error(er);
+      });
+    };
+
+    cond.trigger = function(msg){
+      this.scheduler.trigger(msg, 0);
+    };
+
+    cond.addPool = function(name){
+
+    }
+
+    cond.pool = function(name){
+
+    }
+
+    return cond;
   }
-
-  return cond;
 };
 
 module.exports = ClipConductor;
