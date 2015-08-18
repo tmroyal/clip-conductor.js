64d537448aaf6b45e3b4c6b4d7b0dc536c10a5fd
tmroyal <thomas.m.royal@gmail.com>
2015-08-18 09:02:04 -0400

    Added main tests
    Also made the clip conductor into a module

--- src/main.js
+++ src/main.js
@@ -2,35 +2,29 @@ var Server = require('./Server');
 var Player = require('./Player'); 
 var Scheduler = require('./Scheduler');
 
-var ClipConductor = {
-  new: function(){
-    var cond = Object.create(this);
-    cond.audioContext = new AudioContext();
-    cond.server = Server.new();
-    cond.scheduler = Scheduler.new();
-    cond.player = Player.new(
-      cond.server,
-      cond.scheduler,
-      cond.server
-    );
-    
-    return cond; 
-  },
+var ClipConductor = function(){
+  var server = Server.new();
+  var scheduler = Scheduler.new();
+  var player = Player.new( new AudioContext(), scheduler, server);
 
-  addSound: function(msg, soundInfo){
-    return this.player.loadFile(soundInfo)
+  var cond = {};
+
+  cond.addSound = function(msg, soundInfo){
+    return player.loadFile(soundInfo)
     .then(function(){
-      this.scheduler.on(msg, soundInfo);
+      scheduler.on(msg, soundInfo);
     }.bind(this))
     .catch(function(er){
       console.error('ClipConductor: there was a problem loading '+soundInfo.filename);
       console.error(er);
     });
-  },
+  };
 
-  trigger: function(msg){
+  cond.trigger = function(msg){
     this.scheduler.trigger(msg, 0);
   }
+
+  return cond;
 };
 
 module.exports = ClipConductor;
---
+++ src/main_test.js
@@ -0,0 +1,2 @@
+var ClipConductor = require('./main');
+
