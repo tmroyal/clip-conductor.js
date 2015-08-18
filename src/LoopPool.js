7a7bb0703d50c45a0c109a14bde7426d6722188e
tmroyal <thomas.m.royal@gmail.com>
2015-08-17 20:58:39 -0400

    LoopPool sound management

--- src/LoopPool.js
+++ src/LoopPool.js
@@ -1,4 +1,5 @@
 var Scheduler = require('./Scheduler');
+var _ = require('lodash');
 
 var LoopPool = Object.create(Scheduler);
 
@@ -6,6 +7,55 @@ LoopPool.new = function(name){
   var lp = Scheduler.new.call(this);
   lp.name = name;
   return lp;
+};
+
+LoopPool.addSound = function(sound, range){
+  this.recognizedEvents[sound.handle] = {
+    sound: sound, 
+    range: range
+  };
+};
+
+LoopPool.setSound = LoopPool.addSound;
+
+LoopPool.removeSound = function(sound){
+  delete this.recognizedEvents[sound.handle]; 
+};
+
+LoopPool.getSound = function(value){
+  var closestEvent, closestDistance = Infinity;
+  var matchingEvents = [];
+
+  if (_.isEmpty(this.recognizedEvents)) { return; }
+
+  for (sound in this.recognizedEvents){
+    if (this.recognizedEvents.hasOwnProperty(sound)){
+      var recEvent = this.recognizedEvents[sound];
+      var range = recEvent.range;
+
+      if( value >= range.min && value <= range.max){
+        matchingEvents.push(recEvent);
+      }
+
+      if (_.isEmpty(this.matchingEvents)){
+        var currentDistance = Math.min(
+            Math.abs(value-range.min), 
+            Math.abs(value-range.max)
+        );
+
+        if (currentDistance < closestDistance){
+          closestEvent = recEvent;
+          closestDistance = currentDistance;
+        }
+      }
+    }
+  }
+  if (matchingEvents.length > 0){
+    var ind = Math.floor(Math.random()*matchingEvents.length);
+    return matchingEvents[ind].sound;
+  } else {
+    return closestEvent.sound;
+  }
 };
 
 module.exports = LoopPool;
--- src/LoopPool_test.js
+++ src/LoopPool_test.js
@@ -3,17 +3,104 @@ var LoopPool = require('./LoopPool');
 
 var loopPool;
 
+var testSound, testRange;
+var testSound2, testRange2;
+
+
 beforeEach(function(){
-  loopPool = LoopPool.new();
+  loopPool = LoopPool.new('name');
+  testSound = { handle: 'testSound', filename:'testSound.mp3'};
+  testRange = { min: 0.1, max: 0.5};
+  testSound2 = { handle: 'testSound2', filename:'testSound2.mp3'};
+  testRange2 = { min: 0.6, max: 0.9};
 });
 
 describe('LoopPool', function(){
-  it('should inherit from scheduler', function(){
-    var scheduler = Scheduler.new();
-    for (prop in scheduler){
-      if (scheduler.hasOwnProperty(prop)){
-        loopPool.hasOwnProperty(prop).should.be.true;
+
+  describe('.new', function(){
+    it('should inherit from scheduler', function(){
+      var scheduler = Scheduler.new();
+      for (prop in scheduler){
+        if (scheduler.hasOwnProperty(prop)){
+          loopPool.hasOwnProperty(prop).should.be.true;
+        }
       }
-    }
+    });
+
+    it('should set name property', function(){
+      loopPool = LoopPool.new('name');
+      loopPool.name.should.equal('name');
+    });
+
   });
+
+  describe('.addSound', function(){
+
+    it('should add soundinfo to recognizedEvents object', function(){
+      loopPool.addSound(testSound, testRange);
+      loopPool.recognizedEvents.should.deep.equal(
+        {'testSound':{sound:testSound, range:testRange}}
+      );
+    });
+
+  });
+
+  describe('.setSound', function(){
+    it('should alias setSound', function(){
+      LoopPool.addSound.should.equal(LoopPool.setSound);
+    });
+  });
+
+  describe('.getSound', function(){
+
+    beforeEach(function(){
+      loopPool.addSound(testSound, testRange);
+      loopPool.addSound(testSound2, testRange2);
+    });
+
+    it('should return the sound whose range encompasses value', 
+      function(){
+        var result = loopPool.getSound(0.2);
+        result.should.deep.equal(testSound);
+      }
+    );
+
+    it('should return sound whose range is nearest value if '+
+       ' no range encompasses value', 
+       function(){
+         var result = loopPool.getSound(0.59);
+         console.log(result, testSound);
+         result.should.deep.equal(testSound2);
+       }
+    );
+
+    it('should return undefined if pool is empty', function(){
+      loopPool.recognizedEvents = {};
+      expect(loopPool.getSound(0.2)).to.be.undefined;
+    });
+
+    it('should return a random matching value if mulitple'+
+       ' sound ranges encompass value',
+        function(){
+          var testSound3 = { 
+            handle: 'testSound3', 
+            sound:'testSound3.mp3'
+          };
+
+          sinon.stub(Math, 'random').returns(0.75);
+
+          loopPool.addSound(testSound3, testRange);
+          loopPool.getSound(0.2).should.equal(testSound3);
+        }
+    );
+  });
+
+  describe('.removeSound', function(){
+    it('should remove the sound', function(){
+      loopPool.addSound(testSound, testRange);
+      loopPool.removeSound(testSound);
+      loopPool.recognizedEvents.should.deep.equal({});
+    });
+  });
+
 });
